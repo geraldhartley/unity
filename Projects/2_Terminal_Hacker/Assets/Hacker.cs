ce6a7a08b5d0279f131e1e3c81d882ff5d66f9cd
G C Hartley <traderjoes@vodafone.co.nz>
2018-06-23 16:24:05 +1200

    Initial Commit

---
+++ Projects/2_Terminal_Hacker/Assets/Hacker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        ShowMainMenu();
+	}
+
+    void ShowMainMenu()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("YOU HAVE AWOKEN THE ALMIGHTY SHENRON!\nSPEAK YOUR WISH NOW!");
+        Terminal.WriteLine("----------------------------");
+        Terminal.WriteLine("1. Give me a new partner!");
+        Terminal.WriteLine("2. I want a strong opponent!");
+        Terminal.WriteLine("3. I need some new moves!");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
